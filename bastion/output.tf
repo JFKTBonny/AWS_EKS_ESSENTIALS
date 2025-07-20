fb4b573e6475061d096b8e6a7d5d6de1cafe85a4
JFKTBonny <jkamkotoyip@yahoo.com>
2025-07-20 11:20:56 +0200

    fix bastion/output.tf content

--- bastion/output.tf
+++ bastion/output.tf
@@ -1,11 +1,9 @@
 
 output "subnets" {
   value = data.aws_availability_zones.available.names[1]
-<<<<<<< HEAD
-=======
+
 }
 
 output "public_ip" {
-  value = aws_instance.instances.public_ip
->>>>>>> 78964a0 (initial commit)
+  value = aws_instance.instances.public_
 }
